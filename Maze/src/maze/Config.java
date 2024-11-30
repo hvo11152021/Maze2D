bb8d6689cf13b3b2aa516efd1adca9cf4d6e6263
Hy Vo <94402902+hvo11152021@users.noreply.github.com>
2024-11-30 15:22:14 -0600

    practice: Created wall and floors with 2D array

--- Maze/src/maze/Config.java
+++ Maze/src/maze/Config.java
@@ -15,11 +15,36 @@ import javax.swing.Timer;
  *
  * @author Hy
  */
-public class Config extends JPanel {
+public class Config extends JPanel implements ActionListener {
+    private Timer timer;
+    private Map m;
+    private char[][] map;
+    
+    public Config(){
+        m = new Map();
+        
+        timer = new Timer(25, this);
+        timer.start();
+    }
+    
+    public void actionPerformed(ActionEvent e){
+        
+    }
     
     public void paint(Graphics g){
         super.paint(g);
-        g.setColor(Color.green);
-        g.fillRect(0, 0, 32, 32);
+        map = m.getMap();
+//        g.setColor(Color.green);
+//        g.fillRect(0, 0, 32, 32);
+        for (int y = 0; y < 14; y++){
+            for (int x = 0; x < 14; x++){
+                if (map[y][x] == 'f'){
+                    g.drawImage(m.makeFloor(), x * 32, y * 32, null);
+                }
+                if (map[y][x] == 'w'){
+                    g.drawImage(m.makeWall(), x * 32, y * 32, null);
+                }
+            }
+        }
     }
 }
--- Maze/src/maze/Map.java
+++ Maze/src/maze/Map.java
@@ -14,16 +14,17 @@ import javax.swing.ImageIcon;
  * @author Hy
  */
 public class Map {
-    private Scanner m;
+//    private Scanner m;
+    private char[][] map;
     private String Map[] = new String[14];
     private Image floor;
     private Image wall;
     
     public Map(){
         // assign new vairables with sprites in resources
-        ImageIcon image = new ImageIcon("//");
+        ImageIcon image = new ImageIcon("C://Users//Hy//Documents//GitHub//Maze2D//Maze//resources//floor.png");
         floor = image.getImage();
-        image = new ImageIcon("//");
+        image = new ImageIcon("C://Users//Hy//Documents//GitHub//Maze2D//Maze//resources//wall.png");
         wall = image.getImage();
         
         openFile();
@@ -31,15 +32,34 @@ public class Map {
         closeFile();
     }
     
+    public Image makeFloor() {
+        return floor;
+    }
+    
+    public Image makeWall() {
+        return wall;
+    }
+    
+    public char[][] getMap() {
+        return map;
+    }
+    
     public String getMap(int x, int y){
-        return Map[y].substring(x, x + 1);
+        String index = Map[y].substring(x, x + 1);
+        return index;
     }
 
     private void openFile() {
-        try {
-            m = new Scanner(new File("//"));
+        try (Scanner scanner = new Scanner(new File("C://Users//Hy//Documents//GitHub//Maze2D//Maze//resources//Map.txt"))){
+            map = new char[14][14];
+            int y = 0;
+            while (scanner.hasNextLine()){
+                String line = scanner.nextLine();
+                map[y] = line.toCharArray();
+                y++;
+            }
         } catch(Exception e){
-            System.out.println(e);
+            System.out.println("Error reading the map file: " + e.getMessage());
         }
     }
 
