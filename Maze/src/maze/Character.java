3fdfe26ccfdc4095cc173d37336a7288ef0d1462
Hy Vo <94402902+hvo11152021@users.noreply.github.com>
2024-12-01 23:05:08 -0600

    practice: Created Character file with x y data of 32

---
+++ Maze/src/maze/Character.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package maze;
+
+import java.awt.Image;
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author Hy
+ */
+public class Character {
+    private int x, y, blockX, blockY;
+    private Image character;
+    
+    public Character(){
+        ImageIcon img = new ImageIcon("");
+        character = img.getImage();
+        
+        x = 32;
+        y = 32;
+        
+        blockX = 1;
+        blockY = 1;
+    }
+    
+    public void move(int dx, int dy, int bx, int by){
+    
+    }
+}
